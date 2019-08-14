9e25e1f75123b9717633f53bdbbd3d9f6e30f000
alexandrasheludko <34415638+alexandrasheludko@users.noreply.github.com>
2019-08-14 08:46:37 +0300

    First commit

---
+++ js/common.js
@@ -0,0 +1,32 @@
+ $(function () {
+         $('#datepicker').datepicker({
+             format: "dd_mm_yyyy",
+             autoclose: true,
+             todayHighlight: true,
+         showOtherMonths: true,
+         selectOtherMonths: true,
+         autoclose: true,
+         changeMonth: true,
+         changeYear: true,
+         orientation: "button"
+         });
+     });
+
+  $(function () {
+         $('#datepicker2').datepicker({
+             format: "dd_mm_yyyy",
+             autoclose: true,
+             todayHighlight: true,
+         showOtherMonths: true,
+         selectOtherMonths: true,
+         autoclose: true,
+         changeMonth: true,
+         changeYear: true,
+         orientation: "button"
+         });
+     });
+
+
+
+
+
